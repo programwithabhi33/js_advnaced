e40ca25a4e1e04c7cccb6cc2e7f73c5849763d9f
abhi <programwithabhi@gmail.com>
2022-10-07 20:18:59 +0530

    Add some content

---
+++ test.js
@@ -0,0 +1,22 @@
+let obj ={
+    name:"Abhishek",
+    age:88,
+    hobby:"Software Development",
+    fun1:{
+        name:"Abhishek2",
+        age:88,
+        hobby:"Similar",
+        fun2:function(){
+            console.log(this)
+        },
+        fun3:{
+            name2:"Abhishek3",
+            arr:function(){
+                // console.log("arrow function ")
+                console.log(this)
+            }
+        }
+    }
+}
+obj.fun1.fun2();
+obj.fun1.fun3.arr();
