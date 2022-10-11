3d4305bab53b130a0186602e43dabc2c0db9d69e
abhi <programwithabhi@gmail.com>
2022-10-11 08:18:41 +0530

    Learned the error object

---
+++ error.js
@@ -0,0 +1,13 @@
+try{
+    if(2 == 2){
+       abhi =  new Error('abhishek is the defined')
+    //    throw abhi;
+    }
+    // console.log(abhi)
+    throw new Error('')
+}
+
+// when the error is comes in the script then the catch  block will execute
+catch(error){
+    console.log(abhi)
+}
