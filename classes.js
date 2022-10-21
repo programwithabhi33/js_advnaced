5602ec2e760bb9481316f2c15493eec3afd61cdc
abhi <programwithabhi@gmail.com>
2022-10-21 21:23:54 +0530

    Extends in the javascript

--- classes.js
+++ classes.js
@@ -13,7 +13,22 @@ class Abhi{
     // }
 
 }
-abhishek = new Abhi('abhishek');
+class Abhi2 extends Abhi {
+    constructor(){
+        console.log("This is the constroctor of the Abhi2 class")
+    }
+   another_function(){
+    console.log("this is the another function in the Abhi2 class")
+   }
+
+}
+
+// abhishek = new Abhi('abhishek');
+// // abhishek.fill('abhishek');
+// abhishek.submit();
+// abhishek.cancel();
+
+abhishek2 = new Abhi2('abhi');
 // abhishek.fill('abhishek');
-abhishek.submit();
-abhishek.cancel();
+abhishek2.submit();
+abhishek2.cancel();
