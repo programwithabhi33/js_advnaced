6f13fcca661b6eb713d45ae94b67c21fbb605b0a
abhi <programwithabhi@gmail.com>
2022-10-21 21:01:31 +0530

    Constroctor in the javascript

--- classes.js
+++ classes.js
@@ -1,16 +1,19 @@
 class Abhi{
+    constructor(name){
+        this.name = name;
+    }
     submit(){
         console.log(this.name + ' Your Form has been submitted')
     }
     cancel(){
         console.log(this.name + ' You form has been cancel')
     }
-    fill(name){
-        this.name = name;
-    }
+    // fill(name){
+    //     this.name = name;
+    // }
 
 }
-abhishek = new Abhi();
-abhishek.fill('abhishek');
+abhishek = new Abhi('abhishek');
+// abhishek.fill('abhishek');
 abhishek.submit();
 abhishek.cancel();
