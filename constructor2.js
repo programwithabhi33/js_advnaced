eb69f96ed1bf55686eca657ec926ef176ad3e840
abhi <programwithabhi@gmail.com>
2022-10-26 17:49:29 +0530

    Getter in the javascript class

--- constructor2.js
+++ constructor2.js
@@ -1,22 +1,28 @@
 // console.log('constructor2')
 class Abhi{
-    name
     constructor(name){
-        this.name = name;
-        console.log(name)
+        this._name = name;
+        // console.log(name)
+    }
+    get name(){
+        console.log(`This is the get method in the Abhi class ${this._name}`)
     }
 }
 
-class Abhi2 extends Abhi{
-    constructor(name){
-        // console.log(name)
-        super(name)
-        // super() Note:- You have to call the super class constructo before access the this keyword 
-    }
-    static main2(){
-        console.log('this is the static method in the abhi3 class')
-    }
-}
-let abhishek = new Abhi2('abhishek');
-// console.log(abhishek.name)
-Abhi2.main2();
+// class Abhi2 extends Abhi{
+//     constructor(name){
+//         // console.log(name)
+//         super(name)
+//         // super() Note:- You have to call the super class constructo before access the this keyword 
+//     }
+//     static main2(){
+//         console.log('this is the static method in the abhi3 class')
+//     }
+//     // Main2 is the static method in the Abhi2 class means it is the class member function not inistantiased with the objects and not accessible with the objects 
+// }
+// let abhishek = new Abhi2('abhishek');
+// // console.log(abhishek.name)
+// Abhi2.main2();
+
+let abhishek = new Abhi('abhishek')
+abhishek.name;
