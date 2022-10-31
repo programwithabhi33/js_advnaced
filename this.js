16002c6cbf4d20815dc171342ca66562e6bb10f4
abhi <programwithabhi@gmail.com>
2022-10-31 20:11:19 +0530

    Understand the this in the javascript

--- this.js
+++ this.js
@@ -2,13 +2,13 @@ let obj = {
     name:'abhishek',
     role:"Software Developer",
     exp:2,
-    show:function(){
+    show:function(){ // Normal function will take its current execution context this
         console.log("show")
         console.log(this)
         // function abhi(){
             console.log('abhi')
             console.log(this)
-            let a = ()=>{
+            let a = ()=>{ //arrow function will take its parent this
                 console.log('a')
                 console.log(this)
             }
