efffe2adf0b2dc229a6bc8c8fbcf905fcffc8b56
abhi <programwithabhi@gmail.com>
2022-11-27 23:55:37 +0530

    Understand the hoisting in the javascript

---
+++ hoisting.js
@@ -0,0 +1,12 @@
+// console.log(a)
+// var a = 'abhishek';
+
+// // This is the function expression 
+// const myFunc = ()=>{
+//     console.log("Im a arrow function")
+// }
+
+console.log(abhishek)
+var abhishek = ()=> 2+3;
+
+// The var and the function's that are defined with the function keyword are hoisted at the top of the file during the execucution 
